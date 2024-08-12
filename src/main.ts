4128e6d52bb8b517155c143d6036a9209afa2b6c
Reza Basereh <basereh@mahsan.co>
2024-08-13 02:49:29 +0330

    config reveal js

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,13 @@ import './style.css'
 
 import Reveal from 'reveal.js';
 import Markdown from 'reveal.js/plugin/markdown/markdown.esm.js';
+import RevealHighlight from 'reveal.js/plugin/highlight/highlight.esm';
 
 let deck = new Reveal({
-    plugins: [Markdown]
+    hash: true,
+    minScale: 0.001,
+    maxScale: 10,
+    plugins: [Markdown, RevealHighlight],
 });
 deck.initialize();
 
--- src/presentation.md
+++ src/presentation.md
@@ -7,3 +7,13 @@
 
 ### Client Side Rendering (CSR)
 
+```html
+<!DOCTYPE html>
+<html>
+    <body>
+        <div id="root"></div>
+        <script src="/static/js/bundle.js"></script>
+    </body>
+</html>
+```
+
