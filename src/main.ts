a0237f8f27946b525a795dd3f91fdbd84a1d808c
Reza Basereh <basereh@mahsan.co>
2024-08-13 01:40:02 +0330

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import './style.css'
+
+import Reveal from 'reveal.js';
+import Markdown from 'reveal.js/plugin/markdown/markdown.esm.js';
+
+let deck = new Reveal({
+    plugins: [Markdown]
+});
+deck.initialize();
+
+
---
+++ src/presentation.md
@@ -0,0 +1,9 @@
+# Server Component
+
+
+
+## Server Side Rendering (SSR)
+
+
+### Client Side Rendering (CSR)
+
